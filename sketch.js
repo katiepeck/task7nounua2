2b5a9f8f385f2111f7f6b8ad32e42dff5a717fbe
katiepeck <xkatiemp@hotmail.co.uk>
2017-03-17 14:58:56 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+function setup() {
+  createCanvas(250,250)
+
+}
+function draw() {
+background(255,255,255);
+noStroke();
+
+fill(235,37,26);       //orange
+rect(125,100,50,100);
+
+
+fill(235,37,26);
+rect(75,100,100,50)
+
+fill(235,37,26);          //blue
+rect(75,50,50,100)
+
+fill(28,36,130,205);           //purple
+ellipse(mouseX,75,100, 100);
+
+fill(14,144,96,205);            //purple
+ellipse(mouseY, 175, 100, 100); 
+
+
+}
+
+
+
+
